31b50b87d9041ac4201d7c396e480c65f3e890fa
Justin Cai <justinqcai@gmail.com>
2024-07-02 02:54:24 -0400

    Update title and icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Justin",
+  title: "Justin Cai",
   description: "Justin Cai's Personal Website",
 };
 
